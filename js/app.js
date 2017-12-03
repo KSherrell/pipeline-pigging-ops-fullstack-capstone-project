0105665540f7763a53c83d3924611c052c1db666
Kimzilla <kim.fsw@gmail.com>
2017-12-03 15:05:01 -0700

    created app.js file; created doc.ready function, appLogin function with toggle/hide

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 "use strict";
+
+$(document).ready(function () {
+    $(".jsHide").hide();
+    appLogin();
+});
+
+function toggleHide(e) {
+
+}
+
+function appLogin() {
+
+    $("#pageLogin").toggle();
+    $("#forgotPassword").hide();
+
+    $("p:first-of-type").click(function () {
+
+        console.log("Merry Christmas @ click function");
+
+        $("#forgotPassword").toggle();
+
+        console.log($("#forgotPassword").css("visibility"));
+
+    });
+
+    console.log("Merry Christmas @ login");
+}
