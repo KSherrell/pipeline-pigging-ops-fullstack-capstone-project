c1651f84368908caa2feaa2aef46d045e40e2653
ksherrell <kim.fsw@gmail.com>
2017-12-20 19:30:53 -0700

    first commit from Linux

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://admin:admin@ds133856.mlab.com:33856/pipeline-pigging-ops';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:admin@ds133856.mlab.com:33856/pipeline-pigging-ops';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 3000;
--- server.js
+++ server.js
@@ -1,1 +1,63 @@
+const User = require('./models/user');
+const express = require('express');
+const path = require('path');
+const config = require('./config');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+//const LocalStrategy = require('passport-local').Strategy;
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/pipeline-pigging-ops-fullstack-capstone-project');
+const db = mongoose.connection;
 
+const moment = require('moment');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const BasicStrategy = require('passport-http').BasicStrategy;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public'));
+mongoose.Promise = global.Promise;
+
+
+// ---------------- RUN/CLOSE SERVER -----------------------------------------------------
+let server = undefined;
+
+function runServer(urlToUse) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(urlToUse, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(config.PORT, () => {
+                console.log(`Listening on localhost:${config.PORT}`);
+                resolve();
+            }).on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer(config.DATABASE_URL).catch(err => console.error(err));
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    }));
+}
