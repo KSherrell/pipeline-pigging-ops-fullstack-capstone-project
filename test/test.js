f58e29a3545c719548de81370ef773ac154962ec
K Sherrell <kim.fsw@gmail.com>
2018-03-03 12:41:28 -0700

    troubleshooting Ruby error

--- test/test.js
+++ test/test.js
@@ -19,28 +19,29 @@ const {
 
 chai.use(chaiHttp);
 
-describe('Pigging Ops', function() {
-    before(function() {
+describe('Pigging Ops', function () {
+    before(function () {
         return runServer();
     });
     after(funtion() {
-          return closeServer();
-          });
-
-it('should list pipelines on GET', function(){
-    return chai.request(app)
-    .get('/pipelines')
-    .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res).to.be.a('array');
-        expect(res.body.length).to.be.above(0);
-        res.body.forEach(function(item) {
-            expect(item).to.be.a('object');
-            expect(item).to.have.keys(
-                'RCName', 'systemName', 'pipelineName', 'launcherName', 'receiverName', 'pipelineSize', 'product', 'acceptablePigs', 'closure', 'piggingFrequency', 'dateAdded', 'pipelineActive')
-        });
+        return closeServer();
     });
-});
+
+    it('should list pipelines on GET', function () {
+        return chai.request(app)
+            .get('/pipelines')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res).to.be.a('array');
+                expect(res.body.length).to.be.above(0);
+                res.body.forEach(function (item) {
+                    expect(item).to.be.a('object');
+                    expect(item).to.have.keys(
+                        'RCName', 'systemName', 'pipelineName', 'launcherName', 'receiverName', 'pipelineSize', 'product', 'acceptablePigs', 'closure', 'piggingFrequency', 'dateAdded', 'pipelineActive')
+                });
+            });
+    });
+
 
 });
